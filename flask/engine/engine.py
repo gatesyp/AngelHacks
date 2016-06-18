f6b37de3066c84203bfa2ae4a6e997ead9a555db
gatesyp <ig11@zips.uakron.edu>
2016-06-18 18:14:22 -0400

    added first half of the engine

--- flask/engine/engine.py
+++ flask/engine/engine.py
@@ -1,20 +1,29 @@
-#from generate import getContent as GC
 from use_alch_api import alchemy
-from HARDCODE import c2
 from find_page import findPage
 from bs4 import BeautifulSoup
+from search import findInfo
+from hpe_speech_to_text import useHaven
+
 	
 class Engine():
     def __init__(self):
         print ("created engine object")
         
     def execute(self, filename):
-        keyword_array=alchemy().use_api(c2)
+        hpe=useHaven()
+        json=hpe.speech_text()
+        text=json["document"][0]["content"]
+        #["actions"][0]["result"]
+        keyword_array=hpe.analyze_content(text)
+        
+        # keyword_array=alchemy().use_api(c2)
         searching=findPage()
-        searching.find_page(keyword_array)
-        x=getContent()
-        print "hello"
-        x.get_questions
+        maxpage=searching.find_page(keyword_array)
+        find=findInfo(maxpage)
+        chapter = find.find_chapter()
+        section = find.find_section()
 
 
 
+# myEng = Engine()
+# myEng.execute("short.html")
