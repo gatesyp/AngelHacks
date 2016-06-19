b016327598c05bb083daeaa54539093f9d9793ce
gatesyp <ig11@zips.uakron.edu>
2016-06-19 09:16:05 -0400

    I'm matt and I got all this to work on my own. I organized and lead this project to success. And I get no credit. And no one listens . and adskfj;a;lskdjfa;sldkfjas;ldkfja;sldkfj
    Signed-off-by: gatesyp <ig11@zips.uakron.edu>

--- flask/main.py
+++ flask/main.py
@@ -26,25 +26,25 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        #if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            AUDIO_FILE = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            jsonString = "hello there"
-            return jsonString
-            #return redirect(url_for('uploaded_file', filename=filename))
-            #speech_to_telxt(AUDIO_FILE)
+#    if request.method == 'POST':
+#        # check if the post request has the file part
+#        if 'file' not in request.files:
+#            flash('No file part')
+#            return redirect(request.url)
+#        file = request.files['file']
+#        #if user does not select file, browser also
+#        # submit a empty part without filename
+#        if file.filename == '':
+#            flash('No selected file')
+#            return redirect(request.url)
+#        if file and allowed_file(file.filename):
+#            filename = secure_filename(file.filename)
+#            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#            AUDIO_FILE = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#            jsonString = "hello there"
+#            return jsonString
+#            #return redirect(url_for('uploaded_file', filename=filename))
+#            #speech_to_telxt(AUDIO_FILE)
     return render_template("index.html")
 
 @app.route('/uploads/<filename>')
@@ -58,9 +58,7 @@ def get_outline():
     @param audio_filename: the name of the audio file for this lecture
     @return: JSON representation of the lecture study guide
     """
-    print (request.form['filename'])
-    audio_filename = request.form['filename']
-    return Engine().execute(audio_filename)
+    return Engine().execute("test.wma")
 
 if __name__ == "__main__":
     app.run(debug=True)
